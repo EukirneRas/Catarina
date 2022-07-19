55397778abe22ad4da7fa594b7d34544b53112e4
EukirneRas <henriquedeandraderodrigues@gmail.com>
2022-07-19 17:48:08 -0300

    Catarina

---
+++ Catarina/Catarina/Src/Comida.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catarina.Src
+{
+    internal class Comida
+    {
+        public Salada Salada { get; set; }
+
+        public Carboidrato Carboidrato { get; set; }
+
+        public Proteina Proteina { get; set; }
+
+        public Comida()
+        {
+
+        }
+
+        public Comida (Salada salada, Carboidrato carboidrato, Proteina proteina)
+        {
+            Salada = salada;
+            Carboidrato = carboidrato;
+            Proteina = proteina;
+        }
+    }
+    
+
+    public enum Salada
+    {
+        Tomate, Alface, Pepino, Acelga, Cenoura
+    }
+    public enum Carboidrato
+    {
+        Arroz, Macarrão, Batata
+    }
+    public enum Proteina
+    {
+        Feijao, Soja, Grao_de_bico, Lentilha
+    }
+
+}
